7352753b9f4044767dc1c7f16d8c73ed01ca23d0
unknown <hunter.barclay18@gmail.com>
2019-01-01 12:28:10 -0800

    Added writer... fuck

--- src/main/java/neospace/NeoWriter.java
+++ src/main/java/neospace/NeoWriter.java
@@ -1,9 +1,29 @@
 package neospace;
 
+import java.net.*;
+
 public class NeoWriter {
 
-    public NeoWriter() {
-        
+    private OutputStreamWriter osw;
+
+    public NeoWriter(OutputStream ou) {
+        osw = new OutputStreamWriter(ou);
+    }
+
+    public void Write(String a) {
+        char[] buf = new char[a.length()];
+        for (int i = 0; i < buf.length; i++) {
+            buf[i] = a.charAt(i);
+        }
+        osw.write(buf);
+    }
+
+    public void Flush() {
+        osw.flush();
+    }
+
+    public void Close() {
+        osw.close();
     }
 
 }
