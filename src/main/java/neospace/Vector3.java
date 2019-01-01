0dd8823eb69a584934889ea9642dd2a1930ceb8f
unknown <hunter.barclay18@gmail.com>
2019-01-01 11:47:17 -0800

    Added Vector3 class

---
+++ src/main/java/neospace/Vector3.java
@@ -0,0 +1,30 @@
+package neospace;
+
+import java.lang.Math;
+
+public class Vector3 {
+
+    public double x, y, z;
+
+    public Vector3() {
+        x = 0;
+        y = 0;
+        z = 0;
+    }
+
+    public Vector3(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getMagnitude() {
+        double d = Math.sqrt((x * x) + (y * y) + (z * z));
+        return d;
+    }
+
+    /*public double getNormalized() {
+
+    }*/
+
+}
